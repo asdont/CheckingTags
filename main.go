86b94b10d68ddf7329b8d9e5da2788a4e3f6ffd6
Dmitry <PanshinDA@yandex.ru>
2021-05-09 22:23:55 +0500

    Added <handlingAErrors>

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open(`urls1.txt`)
+	file, err := os.Open(`urls2.txt`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -64,6 +64,7 @@ func main() {
 //}
 
 func extractingTags(bodyString string) {
+
 	// добыча тегов
 	const (
 		hrefType1 = `<a ([\s\S]*?)a>`         // <a "https://url.ru">text a>
@@ -83,7 +84,6 @@ func extractingTags(bodyString string) {
 			resHref[tagHref] = checksTag
 		}
 	}
-	fmt.Println(resHref)
 
 	resImg := map[string][]bool{}
 	for _, typeImg := range typesImg {
@@ -93,7 +93,9 @@ func extractingTags(bodyString string) {
 			resImg[tagImg] = checksTag
 		}
 	}
-	fmt.Println(resImg)
+
+	handlingAErrors(resHref)
+	handlingImgErrors(resImg)
 }
 
 func cleanedTag(tag string) string {
@@ -103,80 +105,95 @@ func cleanedTag(tag string) string {
 	return tag
 }
 
+//TODO выбросить целые теги
+func handlingAErrors(resHref map[string][]bool) {
+	for tag, tagErrors := range resHref {
+		for i := range tagErrors {
+			if !tagErrors[i] {
+				fmt.Println(tag, tagErrors)
+				break
+			}
+		}
+	}
+}
+
+func handlingImgErrors(tags map[string][]bool) {
+	fmt.Println(tags)
+}
+
 func validationHrefTag(tagHref string) (string, []bool) {
 	tagHref = strings.ToLower(strings.Replace(tagHref, "'", "\"", -1))
 
-	linkHref := regexp.MustCompile(`[href]{3,4}="[\s\S]*?"`).FindString(tagHref)
+	linkHref := regexp.MustCompile(`[href]{3,4}="[\s\S]*?"`).FindString(tagHref) // href="link.url"
 
 	// валидация тега a href
 	var existABegin, existHref, existUrl, existText, existAEnd bool
 
-	/* проверки тега A */
+	// проверки тега A
 	existABegin = strings.HasPrefix(tagHref, "<a ")                              // наличие открывающегося тега a
 	existHref = strings.Contains(tagHref, "href")                                // наличие атрибута href
 	existUrl = len(linkHref) > 7                                                 // наличие url, href="" (7)
 	existText = len(regexp.MustCompile(`>[\s\S]*?</a>`).FindString(tagHref)) > 5 // наличие текста ссылки, ></a> (5)
 	existAEnd = strings.HasSuffix(tagHref, "</a>")                               // наличие закрывающего тега a
-	/* ----------------- */
 
 	// валидация url в тэге a href
 	tagUrl := strings.Replace(linkHref, "href=", "", 1)
 	tagUrl = strings.Trim(tagUrl, "\"")
 
-	correctTagHref := []bool{existABegin, existHref, existUrl, existText, existAEnd}
+	correctnessTagHref := []bool{existABegin, existHref, existUrl, existText, existAEnd}
 
 	correctUrlHref := validationURL(tagUrl)
 	for _, boolResult := range correctUrlHref {
-		correctTagHref = append(correctTagHref, boolResult)
+		correctnessTagHref = append(correctnessTagHref, boolResult)
 	}
 
-	return tagHref, correctTagHref
+	return tagHref, correctnessTagHref
 }
 
 func validationImgTag(tagImg string) (string, []bool) {
 	tagImg = strings.ToLower(strings.Replace(tagImg, "'", "\"", -1))
 
-	linkImg := regexp.MustCompile(`[src]{2,4}="[\s\S]*?"`).FindString(tagImg)
+	linkImg := regexp.MustCompile(`[src]{3}="[\s\S]*?"`).FindString(tagImg) // src="link.url"
 
 	// валидация тега img src
 	var existImg, existSrc, existUrl, existEnding bool
 
-	/* проверки тега IMG */
+	// проверки тега IMG
 	existImg = strings.HasPrefix(tagImg, "<img ") // наличие открывающего тега <img
 	existSrc = strings.Contains(tagImg, "src")    // наличие атрибута src
 	existUrl = len(linkImg) > 6                   // наличие ссылки (src="")(6)
 	existEnding = strings.HasSuffix(tagImg, ">")  // закрытие тега
-	/* ----------------- */
 
 	// валидация url в тэге img
 	tagUrl := strings.Replace(linkImg, "src=", "", 1)
 	tagUrl = strings.Trim(tagUrl, "\"")
 
-	correctTagImg := []bool{existImg, existSrc, existUrl, existEnding}
+	correctnessTagImg := []bool{existImg, existSrc, existUrl, existEnding}
 
 	correctUrlImg := validationURL(tagUrl)
 	for _, boolResult := range correctUrlImg {
-		correctTagImg = append(correctTagImg, boolResult)
+		correctnessTagImg = append(correctnessTagImg, boolResult)
 	}
 
-	return tagImg, correctTagImg
+	return tagImg, correctnessTagImg
 }
 
 func validationURL(tagURL string) []bool {
 	// валидация url относительного(<protocol>://<domain>) и абсолютного типа(/<path>/<path>)
 	var existProtocol, existDomain, existSeparators, existSybolsAllowed, existDot bool
-
 	existProtocol = true                                                         // наличие протокола
 	existSeparators = true                                                       // наличие разделителей
-	existDomain = len(tagURL) > 5                                                // длина ссылки, например vk.ru(5)
-	existSybolsAllowed = !regexp.MustCompile(`[^\w\d:/.#-]`).MatchString(tagURL) // проверка запрещенных символов
-	existDot = strings.Contains(tagURL, ".")                                     // наличие точки
+	existDomain = len(tagURL) > 0                                                // длина ссылки, например vk.ru(5)
+	existSybolsAllowed = !regexp.MustCompile(`[^\w:/.#-=?]`).MatchString(tagURL) // проверка запрещенных символов
+	existDot = true                                                              // наличие точки
 
 	isAbsolute := regexp.MustCompile(`:/|/{2}|[htps]{4,5}`).MatchString(tagURL)
 	if isAbsolute {
 		existProtocol = strings.Contains(tagURL, "http")
 		existSeparators = strings.Count(tagURL, "/") >= 2
+		existDot = strings.Contains(tagURL, ".") // наличие точки
+
 	}
 
-	return []bool{existProtocol, existDomain, existSeparators, existSybolsAllowed, existDot}
+	return []bool{existProtocol, existSeparators, existDomain, existSybolsAllowed, existDot}
 }
