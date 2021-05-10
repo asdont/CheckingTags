8ce1a604e6d95e9cdebb635e41434fe75bbdb167
Dmitry <PanshinDA@yandex.ru>
2021-05-10 20:28:53 +0500

    Added <displayingErrors>

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open(`urls2.txt`)
+	file, err := os.Open(`urls1.txt`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,25 +54,27 @@ func main() {
 
 func extractingTags(bodyHtml string) {
 	// добыча тегов
+
+	// для добавления новых регулярок, для случаев ломаных тегов
 	const (
 		hrefType1 = `<a ([\s\S]*?)a>`         // <a "https://url.ru">text a>
 		hrefType2 = `<[\s\S]href([\s\S]*?)a>` // < href="https://url.ru">text a>
 		imgType1  = `<img([\s\S]*?)>`
 	)
 
-	// для добавления новых регулярок, для случаев ломаных тегов
 	typesHref := []string{hrefType1, hrefType2}
 	typesImg := []string{imgType1}
 
 	//bodyHtmlClean := strings.Join(strings.Fields(bodyHtml), " ")
+	bodyHtml = strings.ToLower(bodyHtml)
 
 	countHref := 0
 	for _, typeHref := range typesHref {
 		listHref := regexp.MustCompile(typeHref).FindAllString(bodyHtml, -1)
-		countHref = countHref + len(listHref)
+		countHref += len(listHref)
 		for _, hrefTag := range listHref {
-			link, checksTag := validationHrefTag(hrefTag)
-			errorHandling("href", hrefTag, link, checksTag, bodyHtml)
+			_, checksTag := validationHrefTag(hrefTag)
+			errorHandling("href", hrefTag, checksTag, bodyHtml)
 		}
 	}
 	fmt.Printf("Найдено %d <a href>\n", countHref)
@@ -80,43 +82,109 @@ func extractingTags(bodyHtml string) {
 	countImg := 0
 	for _, typeImg := range typesImg {
 		listImg := regexp.MustCompile(typeImg).FindAllString(bodyHtml, -1)
-		countImg = countImg + len(listImg)
+		countImg += len(listImg)
 		for _, imgTag := range listImg {
-			link, checksTag := validationImgTag(imgTag)
-			errorHandling("img", imgTag, link, checksTag, bodyHtml)
+			_, checksTag := validationImgTag(imgTag)
+			errorHandling("img", imgTag, checksTag, bodyHtml)
 		}
 	}
 	fmt.Printf("Найдено %d <img src>\n", countImg)
 }
 
-func errorHandling(tagType string, tag string, link string, tagErrors []bool, bodyHtml string) {
+func errorHandling(tagType string, tag string, tagErrors []bool, bodyHtml string) int {
 	// обработка найденых ошибок в тегах
 	bodyHtmlLines := strings.Split(bodyHtml, "\n")
-	//bodyHtml = strings.Trim(bodyHtml, "\n")
 
 	for i := range tagErrors {
+		// если есть ошибки в теге
 		if !tagErrors[i] {
 			positionTag := strings.Index(bodyHtml, tag)
 
+			// поиск позиции тега
 			for i, stroke := range bodyHtmlLines {
 				positionTag = positionTag - len(stroke) - 1
 				if positionTag <= 0 {
 					positionTag = positionTag + len(stroke) + 1
 					fmt.Printf("строка - %d столбец - %d, тег - %s\n", i+1, positionTag, tag)
 
-					switch tagType {
-					case "href":
-						fmt.Print("href\n")
-					case "img":
-						fmt.Print("img")
-					}
-
+					// отображение ошибок
+					displayingErrors(tagType, tagErrors)
 					break
 				}
 			}
 		}
 	}
+	return 1
+}
 
+func displayingErrors(tagType string, tagErrors []bool) {
+	// отображение(расшифровка) ошибок
+	switch tagType {
+	case "href":
+		fmt.Println("проверки ->", tagErrors)
+		if !tagErrors[0] {
+			fmt.Println("отсутствует открывающий тег <a ")
+		}
+		if !tagErrors[1] {
+			fmt.Println("отсутствует атрибут href")
+		}
+		if !tagErrors[2] {
+			fmt.Println("не найден url в атрибуте href или href не соответсвует требованиям")
+		}
+		if !tagErrors[3] {
+			fmt.Println("отсутствует текст ссылки(невидимая ссылка) или неверно указан href")
+		}
+		if !tagErrors[4] {
+			fmt.Println("отсутствует закрывающий тег </a>")
+		}
+
+		if !tagErrors[5] {
+			fmt.Println("отсутствует указание протокола http или https")
+		}
+		if !tagErrors[6] {
+			fmt.Println("отустствуют разделители или меньше чем требуется")
+		}
+		if !tagErrors[7] {
+			fmt.Println("короткая длина ссылки, возможно ссылка неверна")
+		}
+		if !tagErrors[8] {
+			fmt.Println("в ссылке присутствуют запрещённые символы или неверно указан href")
+		}
+		if !tagErrors[9] {
+			fmt.Println("в ссылке не найдена точка, возможно так не должно быть")
+		}
+	case "img":
+		fmt.Println("проверки ->", tagErrors)
+		if !tagErrors[0] {
+			fmt.Println("отсутствует открывающий тег <img ")
+		}
+		if !tagErrors[1] {
+			fmt.Println("отсутствует атрибут src")
+		}
+		if !tagErrors[2] {
+			fmt.Println("не найден url в атрибуте src или src не соответсвует требованиям")
+		}
+		if !tagErrors[3] {
+			fmt.Println("отсутствует закрывающий тег >")
+		}
+
+		if !tagErrors[4] {
+			fmt.Println("отсутствует указание протокола http или https")
+		}
+		if !tagErrors[5] {
+			fmt.Println("отустствуют разделители или меньше чем требуется")
+		}
+		if !tagErrors[6] {
+			fmt.Println("короткая длина ссылки, возможно ссылка неверна")
+		}
+		if !tagErrors[7] {
+			fmt.Println("в ссылке присутствуют запрещённые символы")
+		}
+		if !tagErrors[8] {
+			fmt.Println("в ссылке не найдена точка, возможно так не должно быть")
+		}
+	}
+	fmt.Print("\n")
 }
 
 func validationHrefTag(tagHref string) (string, []bool) {
